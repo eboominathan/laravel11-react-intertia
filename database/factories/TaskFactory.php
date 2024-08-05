5749092bb4340253757fb4e5f29794b9e70b3be8
Boominathan <eboominathan@gmail.com>
2024-08-05 10:38:10 +0530

    migraton completed

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -22,7 +22,7 @@ class TaskFactory extends Factory
             'due_date' => fake()->dateTimeBetween('now', '+1 year'),
             'status' => fake()->randomElement(['pending', 'inprogress', 'completed']),
             'image_path' => fake()->imageUrl(),
-            'prioity' => fake()->randomElement(['low','medium', 'high']),
+            'priority' => fake()->randomElement(['low','medium', 'high']),
             'assigned_user_id' => 1,
             'created_by' => 1,
             'updated_by' => 1,
--- database/migrations/2024_08_05_020224_create_tasks_table.php
+++ database/migrations/2024_08_05_020224_create_tasks_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('status');
             $table->string('priority');
             $table->timestamp('due_date')->nullable();
-            $table->foreignId('assign_user_id')->constrained();
+            $table->foreignId('assigned_user_id')->constrained('users');
             $table->foreignId('created_by')->constrained('users');
             $table->foreignId('updated_by')->constrained('users');
             $table->foreignId('project_id')->constrained('projects');
